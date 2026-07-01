da31cbd7751c036871cd8b92051e10384ac6ca7b
Carson <lokyung.chan@ontariotechu.net>
2026-06-30 23:05:57 -0400

    add explicit CORS headers for preflight requests

--- rule_based_chatbot/server.py
+++ rule_based_chatbot/server.py
@@ -10,8 +10,18 @@ app = Flask(__name__)
 CORS(app, origins="*", methods=["GET", "POST", "OPTIONS"], allow_headers=["Content-Type"])
 
 
-@app.route('/api/chat', methods=['POST'])
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    return response
+
+
+@app.route('/api/chat', methods=['POST', 'OPTIONS'])
 def chat():
+    if request.method == 'OPTIONS':
+        return jsonify({}), 200
     data = request.get_json(silent=True) or {}
     message = data.get('message', '')
     try:
